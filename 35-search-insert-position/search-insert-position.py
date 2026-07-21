d9c5aab1433b8dfd701f33380f4ff5fe2080dca1
Rajshimpi2706 <Shimpiraj27@gmail.com>
2026-07-21 17:22:44 +0530

    Time: 0 ms (100.00%) | Memory: 12.9 MB (65.34%) - LeetSync

--- 35-search-insert-position/search-insert-position.py
+++ 35-search-insert-position/search-insert-position.py
@@ -2,7 +2,6 @@ class Solution(object):
     def searchInsert(self, nums, target):
         left = 0
         right = len(nums) - 1
-
         while left <= right:
 
             mid = (left + right) // 2
