32f83707fbbd72035717cce14b068ff56cbf8491
Rajshimpi2706 <Shimpiraj27@gmail.com>
2026-07-22 14:50:22 +0530

    Time: 4 ms (82.12%) | Memory: 12.4 MB (91.01%) - LeetSync

--- 2-add-two-numbers/add-two-numbers.py
+++ 2-add-two-numbers/add-two-numbers.py
@@ -12,20 +12,14 @@ class Solution(object):
             value1 = l1.val if l1 else 0
             value2 = l2.val if l2 else 0
             total = value1 + value2 + carry
-
             carry = total // 10
             digit = total % 10
-
             current.next = ListNode(digit)
-
             current = current.next
-
             if l1:
                 l1 = l1.next
-
             if l2:
                 l2 = l2.next
-
         return dummy.next
         """
         :type l1: Optional[ListNode]
