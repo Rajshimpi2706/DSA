da09c22880dcfcb3f1c57a87010be8bdf7cae575
Rajshimpi2706 <Shimpiraj27@gmail.com>
2026-07-23 21:01:45 +0530

    Time: 3 ms (73.76%) | Memory: 12.6 MB (37.18%) - LeetSync

--- 20-valid-parentheses/valid-parentheses.py
+++ 20-valid-parentheses/valid-parentheses.py
@@ -6,16 +6,19 @@ class Solution(object):
             ']':'[',
             '}':'{'
         }
+        
         for i in s:
             if i in "([{":
                 stack.append(i)
-                
+
             else:
                 if not stack:
                     return False
                 top = stack.pop()
+
                 if top != mapping[i]:
                     return False
+
         return len(stack) == 0
         """
         :type s: str
