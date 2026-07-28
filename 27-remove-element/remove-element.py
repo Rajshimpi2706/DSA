603c6266ce6734796c5e99933623ed065195aa38
Rajshimpi2706 <Shimpiraj27@gmail.com>
2026-07-28 21:52:13 +0530

    Time: 0 ms (100.00%) | Memory: 12.4 MB (53.47%) - LeetSync

--- 27-remove-element/remove-element.py
+++ 27-remove-element/remove-element.py
@@ -1,6 +1,5 @@
 class Solution(object):
     def removeElement(self, nums, val):
-        
         left = 0
 
         for right in range(len(nums)):
@@ -8,7 +7,6 @@ class Solution(object):
             if nums[right] != val:
                 nums[left] = nums[right]
                 left +=1
-
         return left
         """
         :type nums: List[int]
